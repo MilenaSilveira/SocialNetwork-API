ce44df55cc06bc2d90b8f938ef6337c5c283eb24
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-02-23 18:49:30 -0500

    throute

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const {
+  findThoughts,
+  findOneThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(findThoughts).post(createThought);
+
+router
+  .route("/:id")
+  .get(findOneThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
